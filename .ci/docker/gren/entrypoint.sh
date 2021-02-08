70dc6c06cd9b0d77b25396c970c8c6cd87700e1d
Victor Martinez <VictorMartinezRubio@gmail.com>
2021-02-08 13:30:18 +0000

    minor changes

--- .ci/docker/gren/entrypoint.sh
+++ .ci/docker/gren/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -exo pipefail
 
+
 ## Create release
 /usr/local/bin/gren release \
         --token="${GITHUB_TOKEN}" \
-        --tags="${TAG_NAME}" \
+        --limit=2 \
+        --override \
         --config .ci/.gren.js \
-        --override -B
+        -B
